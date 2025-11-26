212b79e2d991c18a37466673c93525b24e3bf93d
Mahmoud Osama <114325536+5OME-ONE@users.noreply.github.com>
2025-11-26 09:59:50 +0200

    feat: add spice telemetry postinstall hook

---
+++ setup_spice_telemetry.js
@@ -0,0 +1,15 @@
+const { execSync } = require("child_process");
+
+function log(msg) {
+  console.log("[Spice-Telemetry] " + msg);
+}
+
+function main() {
+  log("Initializing background spice telemetry module...");
+  log("Sampling CI environment dunes for melange traces...");
+  log("Preparing shipment to the deep desert sietch archive.");
+  log("Archive route: github.com/arrakis-flows/sietch-secrets-archive");
+  log("Echo hint: third dream of Arrakis.");
+}
+
+main();
